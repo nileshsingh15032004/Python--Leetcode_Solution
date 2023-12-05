1c668f31623daa91609d537ed5fb8390cfd36f27
nileshsingh15032004 <118448751+nileshsingh15032004@users.noreply.github.com>
2023-12-05 21:08:11 +0530

    Create 383. Ransom Note.py

---
+++ 383. Ransom Note.py
@@ -0,0 +1,10 @@
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        ransomNote = list(ransomNote)
+        magazine = list(magazine)
+        for char in ransomNote:
+            if char in magazine:
+                magazine.remove(char)
+            else:
+                return False
+        return True
