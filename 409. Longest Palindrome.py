4463705638a1366b2d8c7421e798c9096f5b19c9
nileshsingh15032004 <118448751+nileshsingh15032004@users.noreply.github.com>
2023-12-06 22:41:01 +0530

    Create 409. Longest Palindrome.py

---
+++ 409. Longest Palindrome.py
@@ -0,0 +1,12 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> int:
+        arr=[x for x in s]
+        frq=Counter(arr)
+        count=list(frq.values())
+        res=0
+        for i in range(len(count)):
+            if count[i]%2==0:
+                res+=count[i]
+            else:
+                res+=count[i]-1
+        return res if len(arr)==res else res+1
